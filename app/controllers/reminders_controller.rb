f24e19b8fdef0d090895e5cf61135192682187e7
hbhunt <hbhunt@gmail.com>
2014-01-13 16:42:53 -0500

    Now showing reminders, delete links.

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,6 +1,6 @@
 class RemindersController < ApplicationController
 	before_filter :authenticate_user!, only: [:create, :destroy]
-	# before_action :correct_user, only: :destroy
+	before_action :correct_user, only: :destroy
 
 	def create
 		@reminder = current_user.reminders.build(reminder_params)
@@ -24,8 +24,8 @@ class RemindersController < ApplicationController
 			params.require(:reminder).permit(:name, :event_date)
 		end
 
-	# 	def correct_user
-	# 		@micropost = current_user.microposts.find_by(id: params[:id])
-	# 		redirect_to root_url if @micropost.nil?
-	# 	end
+		def correct_user
+			@reminder = current_user.reminders.find_by(id: params[:id])
+			redirect_to root_url if @reminder.nil?
+		end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  def show
-  	@user = User.find(params[:id])
-    @reminders = @user.reminders.paginate(page: params[:page])
-  end
+	def show
+		@user = User.find(params[:id])
+    	@reminders = @user.reminders.paginate(page: params[:page])
+  	end
 
 end
